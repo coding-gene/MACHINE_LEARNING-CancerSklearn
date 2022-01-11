f91446f48df5074c3f21610ea1960807f53e39fd
Ivan Andrić <andricivan86@gmail.com>
2022-01-11 20:45:57 +0100

    Initial commit

--- logic/mlAlgorithms.py
+++ logic/mlAlgorithms.py
@@ -1,9 +1,90 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+from sklearn.cluster import KMeans
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn import metrics
+import pandas as pd
+import numpy as np
 
 
 class MlAlgorithms:
 
     def __init__(self):
-        pass
+        self.rfc = None
+        self.svmm = None
+        self.knn = None
+        self.dtc = None
+        self.gnb = None
+        self.lr = None
+        self.km = None
 
-    def test(self):
-        pass
+    def random_forest_classifier(self, training_input, training_output, test_input, test_output, prediction_variables):
+        simple_forest_model = RandomForestClassifier(n_estimators=30, criterion='entropy')  # (criterion='entropy')
+        simple_forest_model.fit(training_input, training_output)
+        prediction_rfc = simple_forest_model.predict(test_input)
+        self.rfc = metrics.accuracy_score(prediction_rfc, test_output)
+
+        featimp = pd.Series(simple_forest_model.feature_importances_, index=prediction_variables).\
+            sort_values(ascending=False)
+        # print(f'\tRandom Forest Classifier variable importance: {round(featimp, 2)}')
+
+        max_features_range = np.arange(1, 6, 1)
+        n_estimators_range = np.arange(10, 210, 10)
+        param_grid = dict(max_features=max_features_range, n_estimators=n_estimators_range)
+        rfc_ht = RandomForestClassifier()
+        rfc_grid = GridSearchCV(estimator=rfc_ht, param_grid=param_grid, cv=5)
+        rfc_grid.fit(training_input, training_output)
+        # print(f'\tHyperparameter tuning for RFC -
+        # best params: {rfc_grid.best_params_} , best score: {rfc_grid.best_score_}')
+
+    def support_vector_machine(self, training_input, training_output, test_input, test_output):
+        svm_model = svm.SVC()
+        svm_model.fit(training_input, training_output)
+        prediction_svm = svm_model.predict(test_input)
+        self.svmm = metrics.accuracy_score(prediction_svm, test_output)
+
+    def k_nearest_neighbors(self, training_input, training_output, test_input, test_output):
+        knn_model = KNeighborsClassifier(n_neighbors=5)
+        knn_model.fit(training_input, training_output)
+        prediction_knn = knn_model.predict(test_input)
+        self.knn = metrics.accuracy_score(prediction_knn, test_output)
+
+    def decision_tree_classifier(self, training_input, training_output, test_input, test_output):
+        dtc_model = DecisionTreeClassifier()
+        dtc_model.fit(training_input, training_output)
+        prediction_dtc = dtc_model.predict(test_input)
+        self.dtc = metrics.accuracy_score(prediction_dtc, test_output)
+
+    def gaussian_nb(self, training_input, training_output, test_input, test_output):
+        gnb_model = GaussianNB()
+        gnb_model.fit(training_input, training_output)
+        prediction_gnb = gnb_model.predict(test_input)
+        self.gnb = metrics.accuracy_score(prediction_gnb, test_output)
+
+    def logistic_regression(self, training_input, training_output, test_input, test_output):
+        lr_model = LogisticRegression()
+        lr_model.fit(training_input, training_output)
+        prediction_lr = lr_model.predict(test_input)
+        self.lr = metrics.accuracy_score(prediction_lr, test_output)
+        lr_cr = classification_report(prediction_lr, test_output)
+        # print(f'\tClassification report: {lr_cr}%')
+
+    def k_means(self, training_input, training_output, test_input, test_output):
+        km_model = KMeans(n_clusters=2)
+        km_model.fit(training_input, training_output)
+        prediction_km = km_model.predict(test_input)
+        self.km = metrics.accuracy_score(prediction_km, test_output)
+
+    def results(self):
+        model_results = pd.DataFrame({
+            'Model': ['Random Forest Classifier', 'Support-vector machine', 'K Nearest Neighbors',
+                      'Decision Tree Classifier', 'GaussianNB', 'Logistic Regression', 'KMeans'],
+            'Rezultat': [round(self.rfc, 2), round(self.svmm, 2), round(self.knn, 2), round(self.dtc, 2),
+                         round(self.gnb, 2), round(self.lr, 2), round(self.km, 2)]})
+        model_results.sort_values(by='Rezultat', ascending=False)
+        return model_results
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from logic.mlAlgorithms import MlAlgorithms
 from logic.plots import Plots
 import pandas as pd
-import numpy as np
 import logging
 import time
 import warnings
+from sklearn.model_selection import train_test_split
 
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', 40)
@@ -22,25 +22,73 @@ logging.info('Job started')
 warnings.filterwarnings("ignore")
 # noinspection PyBroadException
 try:
-    plots = Plots()
-    df = pd.read_csv(r'C:\Users\Ivan\PycharmProjects\ML-CancerSklearn\data\data.csv', header=0)
+    df = pd.read_csv(r'data/data.csv', header=0)
 
-    # Izbacivanje nepotrebnih varijabli
     df.drop('Unnamed: 32', axis=1, inplace=True)
     df.drop('id', axis=1, inplace=True)
-    # print(f'Popis radnih varijabli: {df.columns}')
-    # print(f'Informacije o podatcima: {df.info}')
-    # print(f'Deskriptivna statistika: {round(df.describe(), 2)}')
-    # print(f'Provjera null zapisa: {df.isnull().sum()}')
-    # print(f'Struktura ciljane varijable (dijagnoza): {df.diagnosis.value_counts()}')
-    category_mean = list(df.columns[1:11])  # Radit ću samo s ovim setom
+    print(df.columns)
+    print(df.info)
+    print(round(df.describe(), 2))
+    print(df.isnull().sum())
+    print(df.diagnosis.value_counts())
+
+    category_mean = list(df.columns[1:11])
     category_se = list(df.columns[11:20])
     category_worst = list(df.columns[21:31])
 
     df['diagnosis'] = df['diagnosis'].map({'M': 1, 'B': 0})
 
+    plots = Plots()
     plots.count_plot(df=df)
+    plots.scatter_plot(df=df, category_mean=category_mean)
+    plots.correlation_plot(df=df, category_mean=category_mean)
+    plots.linear_regression_plot(df=df)
 
+    ml = MlAlgorithms()
+    prediction_variables = ['radius_mean', 'area_mean', 'perimeter_mean']
+    training, test = train_test_split(df, test_size=0.2)
+    training_input = training[prediction_variables]
+    training_output = training.diagnosis
+    test_input = test[prediction_variables]
+    test_output = test.diagnosis
+
+    ml.random_forest_classifier(
+                    training_input=training_input,
+                    training_output=training_output,
+                    test_input=test_input,
+                    test_output=test_output,
+                    prediction_variables=prediction_variables)
+    ml.support_vector_machine(
+                    training_input=training_input,
+                    training_output=training_output,
+                    test_input=test_input,
+                    test_output=test_output)
+    ml.k_nearest_neighbors(
+                    training_input=training_input,
+                    training_output=training_output,
+                    test_input=test_input,
+                    test_output=test_output)
+    ml.decision_tree_classifier(
+                    training_input=training_input,
+                    training_output=training_output,
+                    test_input=test_input,
+                    test_output=test_output)
+    ml.gaussian_nb(
+                    training_input=training_input,
+                    training_output=training_output,
+                    test_input=test_input,
+                    test_output=test_output)
+    ml.logistic_regression(
+                    training_input=training_input,
+                    training_output=training_output,
+                    test_input=test_input,
+                    test_output=test_output)
+    ml.k_means(
+                    training_input=training_input,
+                    training_output=training_output,
+                    test_input=test_input,
+                    test_output=test_output)
+    ml.results().to_excel('results.xlsx', index=False)
 except Exception:
     logging.exception('An error occurred during job performing:')
 else:
